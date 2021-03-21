4b99b399b8362bbc4ae07c76bc4265b6f6e3cb20
Telek Benjámin <www.tbeni@gmail.com>
2021-03-22 00:15:44 +0100

    BOR commented Fixed problem with duplicate resources (check)

--- src/map/BillOfResources.java
+++ src/map/BillOfResources.java
@@ -4,46 +4,57 @@ import map.asteroid.Resource;
 import utility.OutputFormatter;
 import java.util.ArrayList;
 
+/**
+ * Class map.BillOfResources
+ * Used as blueprints for builds
+ */
 public class BillOfResources {
 
     private ArrayList<Resource> resourcesNeeded = new ArrayList<>();
 
+    /**
+     * Constructor of class
+     */
     public BillOfResources() { }
-    /*public BillOfResources(ArrayList<Resource> resourcesNeeded) {
+    /*// a much nicer constructor
+    public BillOfResources(ArrayList<Resource> resourcesNeeded) {
         OutputFormatter.OutputCall("create - " + this.toString());
         this.resourcesNeeded = resourcesNeeded;
         OutputFormatter.OutputReturn("return");
     }*/
 
     /**
-     * Add a Resources object to the resources list
+     * Add a Resource object to the resources list
+     * @param resource The resource to add
      */
     public void addResources(Resource resource) {
         this.resourcesNeeded.add(resource);
     }
 
     /**
-     * Remove a Resources object from the resources list
+     * Remove a Resource object from the resources list
+     * @param resource The resource to remove
      */
     public void removeResources(Resource resource) {
         this.resourcesNeeded.remove(resource);
     }
 
     /**
+     * Uses the needed resources for the build (removes them from the owned ones)
      * @param ownedResources The resources that the caller owns. Changed if used.
      * @return boolean Whether the usage was successful or not. If yes, the appropriate resources were removed.
      */
     public boolean use(ArrayList<Resource> ownedResources) {
         OutputFormatter.OutputCall("use("+ ownedResources.toString() +")");
-        if (!check(ownedResources)) {
+        if (!check(ownedResources)) { // check if there are enough resources, to prevent unnecessary removal
             OutputFormatter.OutputReturn("return - false");
             return false;
         }
         for (Resource rn : resourcesNeeded) {
             for (int i = 0; i < ownedResources.size(); ++i)
-                if (rn.isCompatibleWith(ownedResources.get(i))) {
-                    ownedResources.remove(i);
-                    break;
+                if (rn.isCompatibleWith(ownedResources.get(i))) { // check if resource types match
+                    ownedResources.remove(i); // removed the found resource
+                    break; // we are done with this one, don't need to remove others of the same type
                 }
         }
         OutputFormatter.OutputReturn("return - true");
@@ -51,21 +62,28 @@ public class BillOfResources {
     }
 
     /**
+     * Checks whether there is enough owned resources to build the damn thing
      * @param ownedResources The resources that the caller owns.
      * @return boolean Is there enough resources for the build?
      */
-    public boolean check(ArrayList<Resource> ownedResources) { //todo: deep copy of array, to properly check for duplicate
+    public boolean check(ArrayList<Resource> ownedResources) {
         OutputFormatter.OutputCall("check("+ ownedResources.toString() +")");
-        int okCnt = 0;
+
+        // create a deep copy of the input array, because we need to modify it, but just locally
+        ArrayList<Resource> ownedRes = (ArrayList<Resource>) ownedResources.clone();
+        int okCnt = 0; // number of found resources
         for (Resource rn : resourcesNeeded) {
             boolean found = false;
-            for (Resource ro : ownedResources)
-                if (rn.isCompatibleWith(ro))
-                    found = true;
+            for (int i = 0; i < ownedRes.size(); ++i)
+                if (rn.isCompatibleWith(ownedRes.get(i))) { // check compatibility
+                    found = true; // found it :)
+                    ownedRes.remove(i); // remove found resource to prevent a problem with duplicate resources
+                    break;
+                }
             if (!found) break;
-            else ++okCnt;
+            else ++okCnt; // increment the number of found resources
         }
-        boolean resultOk = okCnt == resourcesNeeded.size();
+        boolean resultOk = okCnt == resourcesNeeded.size(); // if it found all the necessary resources, then its okay
         OutputFormatter.OutputReturn("return - "+resultOk);
         return resultOk;
     }
