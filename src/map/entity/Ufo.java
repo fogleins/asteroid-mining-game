6fe4bb23628b7f80b6bc16665a48f2b893a50e6f
goma04 <godormarton04@gmail.com>
2021-04-17 21:42:57 +0200

    UFO implementálása

--- src/map/entity/Ufo.java
+++ src/map/entity/Ufo.java
@@ -1,8 +1,10 @@
 package map.entity;
 
+import map.asteroid.Asteroid;
 import map.asteroid.Resource;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Ufo extends Entity implements Steppable {
     private ArrayList<Resource> resources = new ArrayList<>();
@@ -16,13 +18,23 @@ public class Ufo extends Entity implements Steppable {
         super(name);
     }
 
+
+    /**
+     * A UFO collects the resource of the asteroid if it can, otherwise it moves on
+     */
     @Override
     public void step() {
-        // TODO
+        Resource resource = null;
+        resource = asteroid.mined();
+
+        //If the UFO couldn't mine, it moves on
+        if (resource == null) {
+            Random rnd = new Random();
+            ArrayList<Asteroid> neighbours = this.asteroid.getNeighbours().getAsteroidNeighbours();
+            move(neighbours.get(rnd.nextInt(neighbours.size())));
+        }
+
     }
 
-    // TODO: ez korábban private volt, a tesztelés miatt lett public, majd vissza kell állítani
-    public void mine() {
-        // TODO
-    }
+
 }
