cfa0be4f770eb70f1a27ef395072dbed9d3dd083
fogleins <fogleins@hotmail.hu>
2021-05-04 16:33:40 +0200

    Small adjustments to start window

--- src/control/Main.java
+++ src/control/Main.java
@@ -22,23 +22,23 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel(new BorderLayout(25, 25));
-        JLabel centerSubtitleLabel = new JLabel("Asteroid mining game");
-        centerSubtitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        centerSubtitleLabel.setFont(centerSubtitleLabel.getFont().deriveFont(36.0f));
-        JLabel centerTitleLabel = new JLabel("Input players' names");
+        JLabel centerTitleLabel = new JLabel("Asteroid mining game");
         centerTitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        centerTitleLabel.setFont(centerTitleLabel.getFont().deriveFont(24.0f));
+        centerTitleLabel.setFont(centerTitleLabel.getFont().deriveFont(36.0f));
+        JLabel centerSubtitleLabel = new JLabel("Input players' names");
+        centerSubtitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        centerSubtitleLabel.setFont(centerSubtitleLabel.getFont().deriveFont(24.0f));
         JPanel northPanel = new JPanel();
         northPanel.setLayout(new BoxLayout(northPanel, BoxLayout.Y_AXIS));
         northPanel.setBorder(BorderFactory.createEmptyBorder(80, 0, 0, 0));
         JPanel north1 = new JPanel();
         JPanel north2 = new JPanel();
         north2.setBorder(BorderFactory.createEmptyBorder(5, 0, 0, 0));
-        north1.add(centerSubtitleLabel);
-        north2.add(centerTitleLabel);
+        north1.add(centerTitleLabel);
+        north2.add(centerSubtitleLabel);
         northPanel.add(north1);
         northPanel.add(north2);
-        panel.setBorder(BorderFactory.createEmptyBorder(80, 200, 100, 200));
+        panel.setBorder(BorderFactory.createEmptyBorder(60, 200, 120, 200));
         JPanel labels = new JPanel(new GridLayout(0, 1, 2, 2));
         JPanel input = new JPanel(new GridLayout(0, 1, 2, 2));
         ArrayList<JTextField> textFields = new ArrayList<>();
@@ -54,9 +54,10 @@ public class Main {
         panel.add(labels, BorderLayout.WEST);
 
         JPanel buttons = new JPanel();
+        buttons.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
         buttons.setLayout(new BorderLayout());
         JButton startGameButton = new JButton("Start game");
-        startGameButton.setFont(startGameButton.getFont().deriveFont(18.0f));
+        startGameButton.setFont(startGameButton.getFont().deriveFont(20.0f));
         buttons.add(startGameButton, BorderLayout.EAST);
         startGameButton.addActionListener(actionEvent -> {
             ArrayList<String> playerNames = new ArrayList<>();
