e0882fbfb35d81b1cce7a5311b8a3211ce046a30
goma04 <godormarton04@gmail.com>
2021-05-05 11:49:59 +0200

    Minor changes (layout set, jlabels added)

--- src/view/SettlerInventoryView.java
+++ src/view/SettlerInventoryView.java
@@ -3,13 +3,25 @@ package view;
 import map.entity.Settler;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SettlerInventoryView extends JPanel {
 
     private Settler settler;
 
+    SettlerInventoryView(){
+
+        setLayout(new GridLayout(0,2));
+        JLabel titleLabel = new JLabel("Inventory");
+        titleLabel.setFont(titleLabel.getFont().deriveFont(20.0f));
+        add(titleLabel);
+        add(new JPanel());
+        add(new JLabel("Name: "));
+    }
+
     public void updateView(Settler currentSettler){
         settler = currentSettler;
+
     }
 
 }
