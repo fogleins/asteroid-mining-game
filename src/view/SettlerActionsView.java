cc3147de9bcde56f28ced53bf52799bef3ee803d
goma04 <godormarton04@gmail.com>
2021-05-05 11:53:22 +0200

    JButtons added

--- src/view/SettlerActionsView.java
+++ src/view/SettlerActionsView.java
@@ -7,6 +7,22 @@ import javax.swing.*;
 public class SettlerActionsView extends JPanel {
     private Settler settler;
 
+    SettlerActionsView(){
+        JButton moveBtn = new JButton("Move");
+        JButton drillBtn = new JButton("Drill");
+        JButton mineBtn = new JButton("Mine");
+        JButton placeTeleportBtn = new JButton("Build Teleport");
+        JButton buildRobotBtn = new JButton("Build Robot");
+        JButton buildTeleportBtn = new JButton("Place Teleport");
+
+        add(moveBtn);
+        add(drillBtn);
+        add(mineBtn);
+        add(placeTeleportBtn);
+        add(buildRobotBtn);
+        add(buildTeleportBtn);
+    }
+
     public void updateView(Settler settler) {
         this.settler = settler;
     }
