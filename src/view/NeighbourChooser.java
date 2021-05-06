a40f020f9dc777151a0c22804d2d47b50e3bd20b
goma04 <godormarton04@gmail.com>
2021-05-06 12:44:32 +0200

    Class created

---
+++ src/view/NeighbourChooser.java
@@ -0,0 +1,10 @@
+package view;
+
+import map.asteroid.Asteroid;
+import map.asteroid.Neighbours;
+
+public class NeighbourChooser {
+    public Asteroid ChooseAsteroid(Neighbours neighbours){
+        return new Asteroid();
+    }
+}
