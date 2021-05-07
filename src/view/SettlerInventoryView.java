2b43a20267c7811d5906f43b668d454c3db44247
goma04 <godormarton04@gmail.com>
2021-05-07 12:47:10 +0200

    UpdateView implemented, change in design

--- src/view/SettlerInventoryView.java
+++ src/view/SettlerInventoryView.java
@@ -1,6 +1,7 @@
 package view;
 
 import map.entity.Settler;
+import map.resource.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,20 +9,55 @@ import java.awt.*;
 public class SettlerInventoryView extends JPanel {
 
     private Settler settler;
+    JTextArea informationLabel;
+    JLabel titleLabel;
 
-    SettlerInventoryView(){
-
-        setLayout(new GridLayout(0,2));
-        JLabel titleLabel = new JLabel("Inventory");
+    SettlerInventoryView() {
+        titleLabel = new JLabel();
+        informationLabel = new JTextArea();
         titleLabel.setFont(titleLabel.getFont().deriveFont(20.0f));
+
+        informationLabel.setFont(informationLabel.getFont().deriveFont(20.0f));
+        setPreferredSize(new Dimension(200, 500));
         add(titleLabel);
-        add(new JPanel());
-        add(new JLabel("Name: "));
+        add(informationLabel);
+        titleLabel.setPreferredSize(new Dimension(180, 50));
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        titleLabel.setVerticalAlignment(SwingConstants.TOP);
+        informationLabel.setPreferredSize(new Dimension(180, 170));
+        informationLabel.setEditable(false);
+        informationLabel.setHighlighter(null);
+        informationLabel.setBackground(new Color(200, 200, 200, 255));
+        updateView(new Settler("TeszTomi")); //TODO remove this
     }
 
-    public void updateView(Settler currentSettler){
+    public void updateView(Settler currentSettler) {
         settler = currentSettler;
+        titleLabel.setText(settler.getName());
 
+        Uranium uranium = new Uranium();
+        Ice ice = new Ice();
+        Coal coal = new Coal();
+        Iron iron = new Iron();
+
+        BillOfResources bill = new BillOfResources();
+        bill.addResources(uranium);
+
+        informationLabel.append("Uranium: " + bill.count(settler.getResources()) + "\n");
+        bill.removeResources(uranium);
+        bill.addResources(coal);
+
+        informationLabel.append("Coal: " + bill.count(settler.getResources()) + "\n");
+        bill.removeResources(coal);
+        bill.addResources(ice);
+
+        informationLabel.append("Ice: " + bill.count(settler.getResources()) + "\n");
+        bill.removeResources(ice);
+        bill.addResources(iron);
+
+        informationLabel.append("Iron: " + bill.count(settler.getResources()) + "\n\n");
+
+        informationLabel.append("Teleports: " + settler.getTeleportNumber());
     }
 
 }
