5b580a35ac71cb8317bce99f2f3573ec1b26c742
fogleins <fogleins@hotmail.hu>
2021-05-08 01:27:11 +0200

    Clean up AsteroidStatusView

--- src/view/AsteroidStatusView.java
+++ src/view/AsteroidStatusView.java
@@ -6,55 +6,54 @@ import javax.swing.*;
 import java.awt.*;
 
 public class AsteroidStatusView extends JPanel {
-    JLabel titleLabel = new JLabel();
-    //JLabel detailsLabel = new JLabel();
-    JTextArea detailsLabel = new JTextArea();
+    private final JLabel titleLabel = new JLabel();
+    private final JTextArea detailsTextArea = new JTextArea();
 
-    public AsteroidStatusView(){
+    public AsteroidStatusView() {
         titleLabel.setFont(titleLabel.getFont().deriveFont(35.0f));
-        detailsLabel.setFont(detailsLabel.getFont().deriveFont(20.0f));
+        detailsTextArea.setFont(detailsTextArea.getFont().deriveFont(20.0f));
         setPreferredSize(new Dimension(200, 500));
         add(titleLabel);
-        add(detailsLabel);
+        add(detailsTextArea);
         titleLabel.setPreferredSize(new Dimension(180, 50));
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
         titleLabel.setVerticalAlignment(SwingConstants.TOP);
-        detailsLabel.setPreferredSize(new Dimension(180, 450));
-        //detailsLabel.setVerticalAlignment(SwingConstants.TOP);
-        detailsLabel.setEditable(false);
-        detailsLabel.setHighlighter(null);
-        detailsLabel.setBackground(new Color(200, 200, 200, 255));
-        //detailsLabel.setBackground(UIManager.getColor("Panel.background"));
+        detailsTextArea.setPreferredSize(new Dimension(180, 450));
+        //detailsTextArea.setVerticalAlignment(SwingConstants.TOP);
+        detailsTextArea.setEditable(false);
+        detailsTextArea.setHighlighter(null);
+        detailsTextArea.setBackground(new Color(200, 200, 200, 255));
+        //detailsTextArea.setBackground(UIManager.getColor("Panel.background"));
     }
 
-    public void updateView(Asteroid asteroid){
-        if(asteroid == null){
-            return;
-        }
-        GetNameText(asteroid);
-        String details = "";
-        details += GetAsteroidInfo(asteroid);
-        details += GetEntityInfo(asteroid);
-        details += GetTeleportInfo(asteroid);
-        detailsLabel.setText(ToMultiline(details));
-    }
-
-    public static String ToMultiline(String orig)
-    {
+    public static String toMultiline(String orig) {
         return orig.replaceAll("\n", System.lineSeparator()).replaceAll("\t", "      ");
     }
 
-    private String GetTeleportInfo(Asteroid asteroid) {
+    public void updateView(Asteroid asteroid) {
+        if (asteroid == null) {
+            return;
+        }
+        getNameText(asteroid);
+        String details = "";
+        details += getAsteroidInfo(asteroid);
+        details += getEntityInfo(asteroid);
+        details += getTeleportInfo(asteroid);
+        detailsTextArea.setText(toMultiline(details));
+    }
+
+    private String getTeleportInfo(Asteroid asteroid) {
         String res = "";
         res += "Teleport:\n";
-        res += asteroid.getTeleportGate() != null ? (asteroid.getTeleportGate().getOtherSide() == null ? "\t->" + asteroid.getTeleportGate().getOtherSide().getName() + "\n" : "\t-\n") : "\t-\n";
+        res += asteroid.getTeleportGate() != null ? (asteroid.getTeleportGate().getOtherSide() == null ? "\t->" +
+                asteroid.getTeleportGate().getOtherSide().getName() + "\n" : "\t-\n") : "\t-\n";
         return res;
     }
 
-    private String GetEntityInfo(Asteroid asteroid) {
+    private String getEntityInfo(Asteroid asteroid) {
         String res = "";
         res += "Entities:\n";
-        if(asteroid.getEntities().size() == 0){
+        if (asteroid.getEntities().size() == 0) {
             res += "\t-\n";
         } else {
             for (int i = 0; i < asteroid.getEntities().size(); i++) {
@@ -64,14 +63,14 @@ public class AsteroidStatusView extends JPanel {
         return res;
     }
 
-    private String GetAsteroidInfo(Asteroid asteroid) {
+    private String getAsteroidInfo(Asteroid asteroid) {
         String res = "";
         res += asteroid.getInPerihelion() ? "In Perihelion\n" : "\n";
         res += "Resource: " + (asteroid.getResource() == null ? "-\n" : asteroid.getResource().toString() + "\n");
         return res;
     }
 
-    private void GetNameText(Asteroid asteroid) {
+    private void getNameText(Asteroid asteroid) {
         titleLabel.setText(asteroid.getName());
     }
 }
